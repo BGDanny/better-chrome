c23ba6ae04abfaecad16ce3d80c76bc228eebc72
BGDanny <dannyxue121@gmail.com>
2021-02-21 21:42:29 -0700

    improve video auto pause

--- background.js
+++ background.js
@@ -1,96 +1,95 @@
-(function () {
-    "use strict";
-    let tab_playing = [];
-    let video_playing = [];
-    chrome.runtime.onInstalled.addListener(function () {
-        let contextMenuItem = {
-            "id": "BetterChrome",
-            "title": "BetterChrome"
-        };
-        chrome.contextMenus.create(contextMenuItem);
-        tab_playing = [];
-        video_playing = [];
-    })
 
 
-    chrome.contextMenus.onClicked.addListener(function (clickData) {
-        if (clickData.menuItemId === "BetterChrome") {
-            execute();
-        }
-    });
-    // chrome.commands.onCommand.addListener(function () {
-    //     execute();
-    // });
-    function execute() {
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                files: ["contextmenu.js"]
-            });
-        });
-    }
-    // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    //     // console.log(sender.tab ?
-    //     //     "from a content script:" + sender.tab.url :
-    //     //     "from the extension");
-    //     if (request.ad === "removeAds") {
-    //         chrome.scripting.insertCSS({
-    //             target: {sender.tab.id},
-    // files:[""]
+
+chrome.runtime.onInstalled.addListener(function () {
+    // tab_playing = [];
+    // netease = [];
+    let contextMenuItem = {
+        "id": "BetterChrome",
+        "title": "BetterChrome"
+    };
+    chrome.contextMenus.create(contextMenuItem);
+    // chrome.tabs.query({ audible: true }, function (tabs) {
+    //     for (let i = 0; i < tabs.length - 1; i++) {
+    //         chrome.scripting.executeScript({
+    //             target: { tabId: tabs[i].id },
+    //             files: ["media.js"]
     //         });
     //     }
     // });
+});
 
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    if (clickData.menuItemId == "BetterChrome") {
+        execute();
+    }
+});
+// chrome.commands.onCommand.addListener(function () {
+//     execute();
+// });
+function execute() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            files: ["contextmenu.js"]
+        });
+    });
+}
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     // console.log(sender.tab ?
+//     //     "from a content script:" + sender.tab.url :
+//     //     "from the extension");
+//     if (request.ad === "removeAds") {
+//         chrome.scripting.insertCSS({
+//             target: {sender.tab.id},
+// files:[""]
+//         });
+//     }
+// });
+let tab_playing = [];
+let muted = [];
 
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        if (request.video === "playing") {
-            tab_playing.push(sender.tab.id);
-            video_playing.push(request.id);
-            if (tab_playing.length > 1 && sender.tab.id === tab_playing[tab_playing.length - 2] && request.id === video_playing[tab_playing.length - 2]) {
-                tab_playing.pop();
-                video_playing.pop();
-            }
-            if (tab_playing.length > 1) {
-                chrome.tabs.sendMessage(tab_playing[tab_playing.length - 2], { pause: true, id: video_playing[tab_playing.length - 2] });
-            }
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tabInfo) {
+    if (changeInfo.audible == true) {
+        if (tab_playing.length > 1) {
+            tab_playing.shift();
         }
-        else if (request.script === true) {
+        tab_playing.push(tabId);
+        if (tab_playing.length > 1 && tab_playing[0] !== tab_playing[1]) {
             chrome.scripting.executeScript({
-                target: { tabId: sender.tab.id },
-                files: ["contextmenu.js"]
+                target: { tabId: tab_playing[0] },
+                files: ["media.js"]
             });
         }
-        // else if (request.video === "ended") {
-        //     let index = tab_playing.indexOf(sender.tab.id);
-        //     while (index) {
-        //         tab_playing.splice(index, 1);
-        //         video_playing.splice(index, 1);
-        //         index = tab_playing.indexOf(tabId);
-        //     }
-        // }
-    });
-    chrome.tabs.onRemoved.addListener(function (tabId) {
+    }
+});
 
-        let index = tab_playing.indexOf(tabId);
-        while (index >= 0) {
-            tab_playing.splice(index, 1);
-            video_playing.splice(index, 1);
-            index = tab_playing.indexOf(tabId);
-        }
-    });
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    let muteID = muted.indexOf(activeInfo.tabId);
+    if (muteID >= 0) {
+        chrome.tabs.update(activeInfo.tabId, { muted: false });
+        muted.splice(muteID, 1);
+    }
+})
 
-    // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tabInfo) {
-    //     console.log(tabId);
-    //     console.log(changeInfo);
-    //     console.log(tabInfo);
-    //     // chrome.scripting.executeScript({
-    //     //     target: { tabId: tabId },
-    //     //     files: ["contextmenu.js"]
-    //     // });
-    // });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.mute == true) {
+        let muteTabID = tab_playing[0];
+        chrome.tabs.update(muteTabID, { muted: true });
+        muted.push(muteTabID);
+    }
+    else if (request.script == true) {
+        chrome.scripting.executeScript({
+            target: { tabId: sender.tab.id },
+            files: ["contextmenu.js"]
+        });
+    }
+});
 
-    chrome.webNavigation.onCompleted.addListener(function (details) {
-        console.log(details);
-    })
-
-})();
+chrome.tabs.onRemoved.addListener(function (tabId) {
+    let index = tab_playing.indexOf(tabId);
+    while (index >= 0) {
+        tab_playing.splice(index, 1);
+        index = tab_playing.indexOf(tabId);
+    }
+});
