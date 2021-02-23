313108b8a7c82b11b6fec87416b02ee23aa02233
BGDanny <dannyxue121@gmail.com>
2021-02-22 23:36:24 -0700

    update options page

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.contextMenus.create(contextMenuItem);
     chrome.storage.sync.set({ newTab: true, scrollBut: true, pause: true });
 });
-
+// action when context menu is clicked
 chrome.contextMenus.onClicked.addListener(function (clickData, tab) {
     if (clickData.menuItemId == "BetterChrome") {
         chrome.tabs.create({ url: clickData.linkUrl, index: tab.index + 1 });
@@ -17,23 +17,11 @@ chrome.contextMenus.onClicked.addListener(function (clickData, tab) {
         });
     }
 });
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     // console.log(sender.tab ?
-//     //     "from a content script:" + sender.tab.url :
-//     //     "from the extension");
-//     if (request.ad === "removeAds") {
-//         chrome.scripting.insertCSS({
-//             target: {sender.tab.id},
-// files:[""]
-//         });
-//     }
-// });
+// detect when a tab is audible
 let tab_playing;
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tabInfo) {
     if (changeInfo.audible == true) {
-        if (tab_playing !== undefined) {
+        if (tab_playing != undefined && tabID != tab_playing) {
             chrome.scripting.executeScript({
                 target: { tabId: tab_playing },
                 files: ["media.js"]
@@ -42,15 +30,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tabInfo) {
         tab_playing = tabId;
     }
 });
-
-// chrome.tabs.onActivated.addListener(function (activeInfo) {
-//     let muteID = muted.indexOf(activeInfo.tabId);
-//     if (muteID >= 0) {
-//         chrome.tabs.update(activeInfo.tabId, { muted: false });
-//         muted.splice(muteID, 1);
-//     }
-// })
-
+// listen to messages from content scripts and injected scripts
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.mute == true) {
         let muteTabID = sender.tab.id;
@@ -71,3 +51,5 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         });
     }
 });
+
+
--- options.js
+++ options.js
@@ -1,3 +1,52 @@
+const tbody = document.getElementById("tbody");
+chrome.storage.sync.get("reminder", function (data) {
+    for (let i = 0; i < data.reminder.length; i++) {
+        let tr = document.createElement("tr");
+        let th = document.createElement("th");
+        th.setAttribute("scope", "row");
+        th.textContent = i + 1;
+        let date_time = document.createElement("td");
+        date_time.textContent = new Date(data.reminder[i].timestamp);
+        let content = document.createElement("td");
+        content.textContent = data.reminder[i].text;
+        let status = document.createElement("td");
+        if (Date.now() > data.reminder[i].timestamp) {
+            status.textContent = "Resolved";
+        }
+        else {
+            status.textContent = "Pending";
+        }
+        let action = document.createElement("td");
+        let deleteBut = document.createElement("button");
+        deleteBut.setAttribute("type", "button");
+        deleteBut.setAttribute("class", "btn btn-outline-danger");
+        deleteBut.setAttribute("style", "color:black;")
+        deleteBut.textContent = "Delete";
+        deleteBut.addEventListener("click", function () {
+            let updated_reminder = data.reminder;
+            updated_reminder.splice(i, 1);
+            chrome.storage.sync.set({ reminder: updated_reminder }, function () {
+                location.reload();
+            });
+        });
+        let archiveBut = document.createElement("button");
+        archiveBut.setAttribute("type", "button");
+        archiveBut.setAttribute("class", "btn btn-outline-success");
+        archiveBut.setAttribute("style", "color:black;margin-left:10px;")
+        archiveBut.textContent = "Archive";
+        archiveBut.addEventListener("click", function () {
+
+        });
+        action.appendChild(deleteBut);
+        action.appendChild(archiveBut);
+        tr.appendChild(th);
+        tr.appendChild(date_time);
+        tr.appendChild(content);
+        tr.appendChild(status);
+        tr.appendChild(action);
+        tbody.appendChild(tr);
+    }
+});
 const newTab = document.getElementById("newTab");
 const scrollBut = document.getElementById("scroll");
 const pause = document.getElementById("pause");
@@ -15,12 +64,34 @@ scrollBut.addEventListener("input", function () {
 pause.addEventListener("input", function () {
     chrome.storage.sync.set({ pause: pause.checked });
 });
-let notifOptions = {
-    type: "basic",
-    iconUrl: "d128.png",
-    title: "Total reset",
-    message: "reset"
-};
-setTimeout(() => {
-    chrome.notifications.create(notifOptions);
-}, 10000);
+// let notifOptions = {
+//     type: "basic",
+//     iconUrl: "images/d128.png",
+//     title: "Total reset",
+//     message: "reset"
+// };
+
+// setTimeout(() => {
+//     chrome.notifications.create(notifOptions);
+// }, 10000);
+// window.addEventListener('beforeunload', (event) => {
+//     // Cancel the event as stated by the standard.
+//     event.preventDefault();
+//     // Older browsers supported custom message
+//     event.returnValue = '';
+// });
+const dateInput = document.getElementById("dateInput");
+const content = document.getElementById("content");
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function () {
+    if (dateInput.value.length > 0 && content.value.length > 0) {
+        const date1 = new Date(dateInput.value);
+        chrome.storage.sync.get({ reminder: [] }, function (data) {
+            let remind = data.reminder;
+            remind.push({ timestamp: Date.parse(date1), text: content.value });
+            chrome.storage.sync.set({ reminder: remind });
+        });
+    }
+});
+
+
